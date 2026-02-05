08f1a2f26048c52edfa336a499cdca3c389e824d
Gabriel Venancio <gabrielvenancio85@gmail.com>
2026-02-05 10:50:50 -0300

    Adicionando rotas Controller

---
+++ src/main/java/com/ecommerce/Controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.ecommerce.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/signin")
+    public String signIn() {
+        return "signin";
+    }
+
+    @GetMapping("/signup")
+    public String signUp() {
+        return "signup";
+    }
+
+    @GetMapping("logout")
+    public String logout() {
+        return "redirect:/";
+    }
+}
